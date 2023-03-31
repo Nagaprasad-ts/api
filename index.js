7e34cd2220dc8b4b76828fd6987c503d8c5157ff
Nagaprasad T S <113327302+Nagaprasad-ts@users.noreply.github.com>
2023-03-31 11:32:15 +0530

    added reverse function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function myFunction() {
     var x = document.getElementById("inputText").value;
+    // reverse the value in x
+    x = x.split("").reverse().join("");
     document.getElementById("demo").innerHTML = x;
 }
