9a28ea92afb5825c25d272d883f2521206bd85dd
Nagaprasad T S <113327302+Nagaprasad-ts@users.noreply.github.com>
2023-03-31 13:53:15 +0530

    added call function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Description: This file contains the code for the index.html page
 // write the function to reverse the string and display it with id demo, take input from id inputText
 function myFunction() {
+    console.log("index.js loaded");
     var x = document.getElementById("inputText").value;
     // reverse the value in x
     y = x.split("").reverse().join("");
     document.getElementById("demo").innerHTML = y;
 }
 
-console.log("index.js loaded");
+myFunction();
