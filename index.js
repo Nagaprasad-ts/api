26265bddefd260aeaf50d873dc9eaa21ae4aa485
Nagaprasad T S <113327302+Nagaprasad-ts@users.noreply.github.com>
2023-03-31 11:40:43 +0530

    fixed the reverse string bug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Description: This file contains the code for the index.html page
+// write the function to reverse the string and display it with id demo, take input from id inputText
 function myFunction() {
     var x = document.getElementById("inputText").value;
     // reverse the value in x
-    x = x.split("").reverse().join("");
-    document.getElementById("demo").innerHTML = x;
+    y = x.split("").reverse().join("");
+    document.getElementById("demo").innerHTML = y;
 }
